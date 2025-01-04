d31089cee8ec48ac7baa1f1bb8ddd581ea066fef
janeskil1525 <janeskil1525@gmail.com>
2025-01-04 10:57:51 +0100

    First commit

---
+++ lib/Daje/Config.pm
@@ -0,0 +1,37 @@
+package Daje::Config;
+use Mojo::Base -base, -signatures;
+
+
+# NAME
+#
+# Daje::Config - It's new $module
+#
+# SYNOPSIS
+#
+#    use Daje::Config;
+#
+# DESCRIPTION
+#
+# Daje::Config is ...
+#
+# LICENSE
+#
+#Copyright (C) janeskil1525.
+#
+#This library is free software; you can redistribute it and/or modify
+#it under the same terms as Perl itself.
+#
+# AUTHOR
+#
+#janeskil1525 E<lt>janeskil1525@gmail.comE<gt>
+#
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+
+
